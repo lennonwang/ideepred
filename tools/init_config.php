2bd2d7aab2af83f9503efdc1031af1b325e01334
lennonwang <lennon.wang@163.com>
2012-10-21 17:30:25 +0800

    update config

--- tools/init_config.php
+++ tools/init_config.php
@@ -7,8 +7,10 @@
  * @author purpen
  */
 define('ANOLE_PROJECT', 'Anole');
-define('ANOLE_LIB_ROOT','/Users/lennon/project/Anole/src');
-define('ANOLE_DATA_ROOT','/Users/lennon/project/ideepred/data');
+define('ANOLE_LIB_ROOT','/opt/workspace/Anole/src');
+define('ANOLE_DATA_ROOT','/opt/workspace/ideepred/data');
+//define('ANOLE_LIB_ROOT','/Users/lennon/project/Anole/src');
+//define('ANOLE_DATA_ROOT','/Users/lennon/project/ideepred/data');
 require ANOLE_LIB_ROOT.'/Anole.php';
 Anole::boot();
 ?>
